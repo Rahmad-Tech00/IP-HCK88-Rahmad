a59cd7bb1c0fa27a59e65db6fcceb5c649353a69
Rahmad-Tech00 <mohamadrahmad00@gmail.com>
2025-10-14 21:31:51 +0700

    Set Up

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route, Navigate, Outlet } from "react-router"
+import HomePage from "./pages/HomePage"
+
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
