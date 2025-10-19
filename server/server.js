ca9ddb4fa989cc7999692bc10f208906d2c81b01
Rahmad-Tech00 <mohamadrahmad00@gmail.com>
2025-10-20 00:54:09 +0700

    update server and client

--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
 // server.js (root)
-require('dotenv').config();
+// require('dotenv').config();
 const app = require('./app');
 
 const PORT = Number(process.env.PORT || 4000);
-const HOST = process.env.HOST || '0.0.0.0';
+const HOST = '127.0.0.1';
 
 app.listen(PORT, HOST, () => {
   console.log(`⚡ Server listening on http://${HOST}:${PORT}`);
-  console.log(`📦 Environment: ${process.env.NODE_ENV || 'development'}`);
-  console.log(`🌐 Client URL: ${process.env.CLIENT_URL || 'not set'}`);
 });
